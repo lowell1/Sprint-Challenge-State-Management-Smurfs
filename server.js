611dd5c962d4eafc860cbf5376e20a029ed2a50a
lowell1 <lowelljacobs@mail.com>
2019-10-11 12:06:18 -0700

    mvp

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ let smurfs = [
     age: 200,
     height: '5cm',
     id: 0
+  },
+  {
+    name: 'Brainey',
+    age: 200,
+    height: '5cm',
+    id: 0
   }
 ];
 server.get('/smurfs', (req, res) => {
